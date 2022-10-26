dec10e1f21cb3224655466b80136732078dcec8f
Lucas Meurer <lucas.meurer@newcubator.com>
2022-10-26 13:15:38 +0200

    Optimized images

--- app/(navbar)/page.tsx
+++ app/(navbar)/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         {data.data?.galleryPosts.map(post => ((
           <div className="card w-96 bg-base-100 shadow-xl m-6" key={post.id}>
             <figure className="h-96 relative object-cover">
-              <Image className="object-cover" src={post.imageUrl!} alt="Post Image" fill unoptimized/>
+              <Image className="object-cover" src={post.imageUrl!} alt="Post Image" fill />
             </figure>
             <div className="card-body">
               <div className="card-title">
